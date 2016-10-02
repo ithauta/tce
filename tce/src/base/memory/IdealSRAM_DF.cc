7d33608683896fe966b4db65dbf0600ce97f33f6
ithauta <ithauta@ee.oulu.fi>
2016-10-02 22:48:46 +0300

    Array based SRAM model added

---
+++ tce/src/base/memory/IdealSRAM_DF.cc
@@ -0,0 +1,115 @@
+/*
+    Copyright (c) 2002-2009 Tampere University of Technology.
+
+    This file is part of TTA-Based Codesign Environment (TCE).
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @file IdealSRAM_DF.cc
+ *
+ * Definition of IdealSRAM_DF class.
+ *
+ * @author Jussi Nykänen 2004 (nykanen-no.spam-cs.tut.fi)
+ * @author Pekka Jääskeläinen 2005 (pjaaskel-no.spam-cs.tut.fi)
+ * @author Ilkka Hautala 2016 (ithauta-no.spam-ee-oulu.fi)
+ * @note rating: red
+ */
+
+#include <string>
+#include <utility>
+
+#include "IdealSRAM_DF.hh"
+#include "Conversion.hh"
+#include "Application.hh"
+
+using std::string;
+
+
+/**
+ * Constructor. Create a model for a given memory.
+ *
+ * The created memory model is empty. No data is allocated for its contents.
+ *
+ * @param start First address of the memory.
+ * @param end Last address of the memory.
+ * @param MAUSize Bit width of the minimum addressable unit of the memory.
+ * @param wordSize Number of MAUs that make up a natural word.
+ * @param align Alignment of natural words, expressed in number of MAUs.
+ */
+IdealSRAM_DF::IdealSRAM_DF(Word start, Word end, Word MAUSize) :
+    Memory(start, end, MAUSize), start_(start), end_(end), MAUSize_(MAUSize) {
+    data_ = new Memory::MAU[end_ - start_];
+}
+
+
+/**
+ * Destructor.
+ *
+ * The storage reserved for the memory contents is deallocated. Any data
+ * about pending access requests is freed, too.
+ */
+IdealSRAM_DF::~IdealSRAM_DF() {
+    delete data_;
+    data_ = NULL;
+}
+
+/**
+ * Writes a single memory location.
+ *
+ * The fastest way to write to the memory.
+ *
+ * @param address The target address.
+ * @param data The data to write.
+ */
+void
+IdealSRAM_DF::write(Word address, MAU data) {
+    data_[address - start_] = data;
+}
+
+/**
+ * Reads a single memory location.
+ *
+ * The fastest way to read the memory.
+ *
+ * @param address The address to read.
+ * @return The data read.
+ */
+Memory::MAU
+IdealSRAM_DF::read(Word address) {
+    return data_[address - start_];
+}
+
+/**
+ * Fills the whole memory with zeros.
+ *
+ * This is needed due to some buggy simulated programs which expect
+ * uninitialized data to be zero.
+ */
+void
+IdealSRAM_DF::fillWithZeros() {
+    data_ = {0};
+}
+
+
+Memory::MAU *
+IdealSRAM_DF::getStoragePointer(){
+    return data_
+}
+
---
+++ tce/src/base/memory/IdealSRAM_DF.hh
@@ -0,0 +1,83 @@
+/*
+    Copyright (c) 2002-2009 Tampere University of Technology.
+
+    This file is part of TTA-Based Codesign Environment (TCE).
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @file IdealSRAM_DF.hh
+ *
+ * Declaration of IdealSRAM_DF class.
+ *
+ * @author Jussi Nykänen 2004 (nykanen-no.spam-cs.tut.fi)
+ * @author Ilkka Hautala 2016 (ithauta-no.spam-ee.oulu.fi)
+ * @note rating: red
+ */
+
+#ifndef TTA_IDEAL_SRAM_DF_HH
+#define TTA_IDEAL_SRAM_DF_HH
+
+#include <vector>
+#include <map>
+
+#include "Memory.hh"
+
+/**
+ * Class that models an "ideal" memory.
+ *
+ * An ideal memory is defined as a memory with read latency zero. 
+ * The data is available at the same cycle in which the load is initiated. 
+ * Also, after a store is initiated, data is written into memory as soon as
+ * the clock advances.
+ *
+ */
+class IdealSRAM_DF : public Memory {
+public:
+    IdealSRAM_DF(Word start, Word end, Word MAUSize);
+    virtual ~IdealSRAM_DF();
+
+    virtual void write(Word address, MAU data);
+    virtual Memory::MAU read(Word address);
+
+    using Memory::write;
+    using Memory::read;
+
+    virtual void fillWithZeros();
+
+    virtual Memory::MAU * getStoragePointer();
+
+
+private:
+    /// Copying not allowed.
+    IdealSRAM_DF(const IdealSRAM_DF&);
+    /// Assignment not allowed.
+    IdealSRAM_DF& operator=(const IdealSRAM_DF&);
+
+    /// Starting point of the address space.
+    Word start_;
+    /// End point of the address space.
+    Word end_;
+    /// Size of the minimum adressable unit.
+    Word MAUSize_;
+    /// Container for holding read/write requests.
+    Memory::MAU * data_;
+};
+
+#endif
